56405547c8c14f80afe178f0f836b0542e561b4a
Matthew Niemiro <29468575+mattniemiro@users.noreply.github.com>
2025-02-09 14:01:48 -0500

    Added Roboto to index.css and tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -87,7 +87,10 @@ export default {
 			animation: {
 				'accordion-down': 'accordion-down 0.2s ease-out',
 				'accordion-up': 'accordion-up 0.2s ease-out'
-			}
+			},
+			fontFamily: {
+				sans: ["Roboto", "sans-serif"],
+			},
 		}
 	},
 	plugins: [require("tailwindcss-animate")],
