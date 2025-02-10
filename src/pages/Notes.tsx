dd61de94bb8924936b67b71fce299f8cde3a3682
Matthew Niemiro <29468575+mattniemiro@users.noreply.github.com>
2025-02-10 09:47:15 -0500

    Twekaing lists

--- src/pages/Notes.tsx
+++ src/pages/Notes.tsx
@@ -4,29 +4,29 @@ const Notes = () => {
   const notes = [
     {
       title: "Ambidexterity and Semiadditivity",
-      subtitle: "Coming soon?",
+      subtitle: "In preparation",
       description: "My paper for the University of Chicago 2023 REU. Thanks to Peter May for advising me.",
       date: "2023",
       link: "#"
     },
     {
       title: "The Adams Conjecture and the K-theory of finite fields",
-      subtitle: "jt with Zhong Zhang",
-      description: "My paper written at the University of Chicago 2022 REU. Thanks to Peter May for advising me.",
+      subtitle: <>Chicago REU 2022, Joint w/ <a href="https://zhongzhang-math.github.io/about/" className="no-underline">Zhong Zhang</a></>,
+      description: "My paper written at the University of Chicago 2022 REU. Thanks to Peter May for advising us.",
       date: "2022",
       link: "#"
     },
     {
       title: "The derived functor approach to sheaf cohomology",
-      subtitle: "",
+      subtitle: "Chicago REU 2021",
       description: "My paper written at the University of Chicago 2021 REU. Thanks to Max Johnson for mentoring me.",
       date: "2021",
       link: "#"
     },
     {
       title: "Introduction to de Rham cohomology",
-      subtitle: "",
-      description: "Notes on de Rham cohomology a long time ago.",
+      subtitle: "Old notes",
+      description: "Notes on de Rham cohomology I wrotea long time ago.",
       date: "2021",
       link: "#"
     }
@@ -34,7 +34,6 @@ const Notes = () => {
 
   return (
     <div className="container max-w-3xl px-4 py-8">
-      <h1 className="text-2xl mb-6">Notes</h1>
       <h2 className="text-xl font-bold mb-4">Unsorted</h2>
       <PaperList papers={notes} />
     </div>
