3f54832bf6e1ad6365315948fbe07bf838e2f971
Matthew Niemiro <29468575+mattniemiro@users.noreply.github.com>
2025-02-10 21:39:15 -0500

    Revert "Added to tailwind.config.js"
    This reverts commit abe56a5dd7917cad65cecce37a4057d1a73a738e.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  // ... existing config ...
   theme: {
     extend: {
       keyframes: {
@@ -12,15 +8,6 @@ module.exports = {
           '50%': { opacity: 0.3 },
         },
       },
-      colors: {
-        background: 'white', // or any color you want for bg-background
-        // If you're using dark mode, you might want to add:
-        dark: {
-          background: '#1a1a1a'
-        }
-      }
     },
   },
-  darkMode: 'class', // if you're using dark mode
-  plugins: [],
 }
