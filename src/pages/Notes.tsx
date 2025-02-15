eb1380cad4e42059e9f2d329e6e779d58c1b5bf4
Matthew Niemiro <29468575+mattniemiro@users.noreply.github.com>
2025-02-15 18:58:35 -0500

    Added pdfs

--- src/pages/Notes.tsx
+++ src/pages/Notes.tsx
@@ -14,21 +14,21 @@ const Notes = () => {
       subtitle: <>Chicago REU 2022, Joint w/ <a href="https://zhongzhang-math.github.io/about/" className="no-underline">Zhong Zhang</a></>,
       description: "My paper written at the University of Chicago 2022 REU. Thanks to Peter May for advising us.",
       date: "2022",
-      link: "#"
+      link: "/docs/reu_2022_paper.pdf"
     },
     {
       title: "The derived functor approach to sheaf cohomology",
       subtitle: "Chicago REU 2021",
       description: "My paper written at the University of Chicago 2021 REU. Thanks to Max Johnson for mentoring me.",
       date: "2021",
-      link: "#"
+      link: "/docs/reu_2021_paper.pdf"
     },
     {
       title: "Introduction to de Rham cohomology",
       subtitle: "Old notes",
       description: "Notes on de Rham cohomology I wrote a long time ago.",
       date: "2021",
-      link: "#"
+      link: "/docs/de_rham_cohomology.pdf"
     }
   ];
 
