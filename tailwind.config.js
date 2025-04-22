f9a722a51a128bb7c0f80d2b3773e303ab321a27
Matthew Niemiro <29468575+mattniemiro@users.noreply.github.com>
2025-04-21 21:00:17 -0400

    Fixed container centering

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ module.exports = {
     './src/**/*.{ts,tsx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
         background: "hsl(var(--background))",
