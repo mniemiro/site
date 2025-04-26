e46fd4df4abab6ac9c4eea595a7c5b0f669c5645
Matthew Niemiro <29468575+mattniemiro@users.noreply.github.com>
2025-04-26 01:56:58 -0400

    Tweak

--- src/pages/Notes.tsx
+++ src/pages/Notes.tsx
@@ -36,7 +36,7 @@ const Notes = () => {
     <div className="max-w-3xl">
       <PaperList papers={notes} />
       <p className="mt-4">
-        Here is a <a href="/docs/Lurie_252x_bookmarks.pdf" className="no-underline">PDF</a> of Jacob Lurie's "Chromatic Homotopy Theory (252x)" lecture notes, with BOOKMARKS.
+        Here is a <a href="/docs/Lurie_252x_bookmarks.pdf" className="no-underline" target="_blank" rel="noopener noreferrer">bookmarked PDF</a> of Jacob Lurie's "Chromatic Homotopy Theory (252x)" lecture notes.
       </p>
     </div>
   );
