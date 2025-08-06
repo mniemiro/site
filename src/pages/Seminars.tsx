8dbd4ca51197f9b3e6a3bc186e1c55e7ee7b4e06
Matthew Niemiro <29468575+mattniemiro@users.noreply.github.com>
2025-08-06 13:35:23 -0500

    Updates Seminars.tsx

--- src/pages/Seminars.tsx
+++ src/pages/Seminars.tsx
@@ -1,10 +1,15 @@
 export const Seminars = () => {
   return (
     <div className="prose dark:prose-invert max-w-none">
-      <h1>Seminars</h1>
       <p className="text-[13px]">
-        Information about seminars I organize and participate in.
+        Here are some seminars I am organizing or have organized.
       </p>
+      <ul className="text-[13px]">
+        <li>(Spring 2025) Harvard/MIT Babytop Seminar --- Trace methods II</li>
+        <li>(Fall 2024) Harvard/MIT Babytop Seminar --- <a href="https://math.mit.edu/topology/babytop/" className="text-link hover:text-link-hover" target="_blank" rel="noopener noreferrer">Trace methods I</a></li>
+        <li>(Spring 2024) Harvard/MIT Babytop Seminar --- <a href="https://math.mit.edu/topology/babytop/" className="text-link hover:text-link-hover" target="_blank" rel="noopener noreferrer">chromatic homotopy theory</a></li>
+        <li>(Summer 2022) Condensed Seminar @UChicago</li>
+      </ul>
     </div>
   );
 };
