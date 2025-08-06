b14f8634b11164e23252385676cfd330a6e4098b
Matthew Niemiro <29468575+mattniemiro@users.noreply.github.com>
2025-08-06 13:28:23 -0500

    Adding a seminars page

---
+++ src/pages/Seminars.tsx
@@ -0,0 +1,12 @@
+export const Seminars = () => {
+  return (
+    <div className="prose dark:prose-invert max-w-none">
+      <h1>Seminars</h1>
+      <p className="text-[13px]">
+        Information about seminars I organize and participate in.
+      </p>
+    </div>
+  );
+};
+
+export default Seminars; 
