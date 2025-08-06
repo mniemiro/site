ee83a277cdb14ffb062abfda0ab4fc364a6a6f0a
Matthew Niemiro <29468575+mattniemiro@users.noreply.github.com>
2025-08-06 13:37:09 -0500

    List formatting

--- src/pages/Seminars.tsx
+++ src/pages/Seminars.tsx
@@ -4,11 +4,11 @@ export const Seminars = () => {
       <p className="text-[13px]">
         Here are some seminars I am organizing or have organized.
       </p>
-      <ul className="text-[13px]">
-        <li>(Spring 2025) Harvard/MIT Babytop Seminar --- Trace methods II</li>
-        <li>(Fall 2024) Harvard/MIT Babytop Seminar --- <a href="https://math.mit.edu/topology/babytop/" className="text-link hover:text-link-hover" target="_blank" rel="noopener noreferrer">Trace methods I</a></li>
-        <li>(Spring 2024) Harvard/MIT Babytop Seminar --- <a href="https://math.mit.edu/topology/babytop/" className="text-link hover:text-link-hover" target="_blank" rel="noopener noreferrer">chromatic homotopy theory</a></li>
-        <li>(Summer 2022) Condensed Seminar @UChicago</li>
+      <ul className="text-[13px] pl-6">
+        <li className="mb-2">(Spring 2025) Harvard/MIT Babytop Seminar --- Trace methods II</li>
+        <li className="mb-2">(Fall 2024) Harvard/MIT Babytop Seminar --- <a href="https://math.mit.edu/topology/babytop/" className="text-link hover:text-link-hover" target="_blank" rel="noopener noreferrer">Trace methods I</a></li>
+        <li className="mb-2">(Spring 2024) Harvard/MIT Babytop Seminar --- <a href="https://math.mit.edu/topology/babytop/" className="text-link hover:text-link-hover" target="_blank" rel="noopener noreferrer">chromatic homotopy theory</a></li>
+        <li className="mb-2">(Summer 2022) Condensed Seminar @UChicago</li>
       </ul>
     </div>
   );
