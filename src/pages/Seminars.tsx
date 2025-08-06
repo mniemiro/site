f4017b6afeb06dd6c73d5ca49e4dff692f075b1d
Matthew Niemiro <29468575+mattniemiro@users.noreply.github.com>
2025-08-06 15:46:44 -0500

    Tweaking list

--- src/pages/Seminars.tsx
+++ src/pages/Seminars.tsx
@@ -4,11 +4,19 @@ export const Seminars = () => {
       <p className="text-[13px] mb-4">
         Here are some seminars I am organizing or have organized.
       </p>
-      <ul className="text-[13px] pl-6">
-        <li className="mb-2">(Spring 2025) Harvard/MIT Babytop Seminar --- Trace methods II</li>
-        <li className="mb-2">(Fall 2024) Harvard/MIT Babytop Seminar --- <a href="https://math.mit.edu/topology/babytop/" className="text-link hover:text-link-hover" target="_blank" rel="noopener noreferrer">Trace methods I</a></li>
-        <li className="mb-2">(Spring 2024) Harvard/MIT Babytop Seminar --- <a href="https://math.mit.edu/topology/babytop/" className="text-link hover:text-link-hover" target="_blank" rel="noopener noreferrer">Chromatic homotopy theory</a></li>
-        <li className="mb-2">(Summer 2022) Condensed Seminar @UChicago</li>
+      <ul className="text-[13px] space-y-2 list-none">
+        <li className="before:content-['(Spring 2026)'] before:inline-block before:w-16 before:text-right before:mr-4 before:font-mono">
+          (Spring 2025) Harvard/MIT Babytop Seminar --- Trace methods II, joint w/ Logan Hyslop
+        </li>
+        <li className="before:content-['(Fall 2025)'] before:inline-block before:w-16 before:text-right before:mr-4 before:font-mono">
+          (Fall 2024) Harvard/MIT Babytop Seminar --- <a href="https://math.mit.edu/topology/babytop/" className="text-link hover:text-link-hover" target="_blank" rel="noopener noreferrer">Trace methods I</a>, joint w/ Logan Hyslop
+        </li>
+        <li className="before:content-['(Spring 2024)'] before:inline-block before:w-16 before:text-right before:mr-4 before:font-mono">
+          (Spring 2024) Harvard/MIT Babytop Seminar --- <a href="https://math.mit.edu/topology/babytop/" className="text-link hover:text-link-hover" target="_blank" rel="noopener noreferrer">Chromatic homotopy theory</a>
+        </li>
+        <li className="before:content-['(Summer 2022)'] before:inline-block before:w-16 before:text-right before:mr-4 before:font-mono">
+          (Summer 2022) Condensed Seminar @UChicago, joint w/ Michael Barz
+        </li>
       </ul>
     </div>
   );
