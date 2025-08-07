a1052eceda19c4446856682cca7cd8572c5aeafd
Matthew Niemiro <29468575+mattniemiro@users.noreply.github.com>
2025-08-07 13:40:45 -0500

    Update Seminars.tsx

--- src/pages/Seminars.tsx
+++ src/pages/Seminars.tsx
@@ -5,9 +5,9 @@ export const Seminars = () => {
         Here are some seminars I am organizing or have organized.
       </p>
       <ul className="text-[13px] space-y-2 pl-6">
-        <li>(Spring 2026) Harvard/MIT Babytop Seminar --- Trace methods II, joint w/ Logan Hyslop</li>
-        <li>(Fall 2025) Harvard/MIT Babytop Seminar --- <a href="https://math.mit.edu/topology/babytop/" className="text-link hover:text-link-hover" target="_blank" rel="noopener noreferrer">Trace methods I</a>, joint w/ Logan Hyslop</li>
-        <li>(Spring 2025) Harvard/MIT Babytop Seminar --- <a href="https://math.mit.edu/topology/babytop/" className="text-link hover:text-link-hover" target="_blank" rel="noopener noreferrer">Chromatic homotopy theory</a></li>
+        <li>(Spring 2026) Harvard/MIT Babytop --- Trace methods II, joint w/ Logan Hyslop</li>
+        <li>(Fall 2025) Harvard/MIT Babytop --- <a href="https://math.mit.edu/topology/babytop/" className="text-link hover:text-link-hover" target="_blank" rel="noopener noreferrer">Trace methods I</a>, joint w/ Logan Hyslop</li>
+        <li>(Spring 2025) Harvard/MIT Babytop --- <a href="https://math.mit.edu/topology/babytop/" className="text-link hover:text-link-hover" target="_blank" rel="noopener noreferrer">Chromatic homotopy theory</a></li>
         <li>(Summer 2022) Condensed Seminar @UChicago, joint w/ Michael Barz</li>
       </ul>
     </div>
