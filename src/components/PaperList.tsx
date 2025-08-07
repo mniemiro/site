b9726f23f28296ae2b965ae4d71723ca192f2f98
Matthew Niemiro <29468575+mattniemiro@users.noreply.github.com>
2025-08-07 13:45:21 -0500

    Update PaperList.tsx

--- src/components/PaperList.tsx
+++ src/components/PaperList.tsx
@@ -40,7 +40,7 @@ export const PaperList = ({ papers }: PaperListProps) => {
     
     if (openIndex !== index) {
       setShowConfetti(true);
-      setTimeout(() => setShowConfetti(false), 4500);
+      setTimeout(() => setShowConfetti(false), 4000);
     }
     setOpenIndex(openIndex === index ? null : index);
   }, [openIndex, papers]);
