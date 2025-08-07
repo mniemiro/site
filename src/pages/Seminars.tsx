2e36477528a3479522e5f67a966189f3a6be1c8e
Matthew Niemiro <29468575+mattniemiro@users.noreply.github.com>
2025-08-06 19:48:29 -0500

    Tweaks

--- src/pages/Seminars.tsx
+++ src/pages/Seminars.tsx
@@ -5,16 +5,16 @@ export const Seminars = () => {
         Here are some seminars I am organizing or have organized.
       </p>
       <ul className="text-[13px] space-y-2 list-none">
-        <li className="before:content-['(Spring 2026)'] before:inline-block before:w-16 before:text-right before:mr-4 before:font-mono">
+        <li className="before:content-['(Spring 2026)'] before:inline-block before:w-12 before:text-right before:mr-4 before:font-mono">
           (Spring 2025) Harvard/MIT Babytop Seminar --- Trace methods II, joint w/ Logan Hyslop
         </li>
-        <li className="before:content-['(Fall 2025)'] before:inline-block before:w-16 before:text-right before:mr-4 before:font-mono">
+        <li className="before:content-['(Fall 2025)'] before:inline-block before:w-12 before:text-right before:mr-4 before:font-mono">
           (Fall 2024) Harvard/MIT Babytop Seminar --- <a href="https://math.mit.edu/topology/babytop/" className="text-link hover:text-link-hover" target="_blank" rel="noopener noreferrer">Trace methods I</a>, joint w/ Logan Hyslop
         </li>
-        <li className="before:content-['(Spring 2025)'] before:inline-block before:w-16 before:text-right before:mr-4 before:font-mono">
+        <li className="before:content-['(Spring 2025)'] before:inline-block before:w-12 before:text-right before:mr-4 before:font-mono">
           (Spring 2024) Harvard/MIT Babytop Seminar --- <a href="https://math.mit.edu/topology/babytop/" className="text-link hover:text-link-hover" target="_blank" rel="noopener noreferrer">Chromatic homotopy theory</a>
         </li>
-        <li className="before:content-['(Summer 2022)'] before:inline-block before:w-16 before:text-right before:mr-4 before:font-mono">
+        <li className="before:content-['(Summer 2022)'] before:inline-block before:w-12 before:text-right before:mr-4 before:font-mono">
           (Summer 2022) Condensed Seminar @UChicago, joint w/ Michael Barz
         </li>
       </ul>
