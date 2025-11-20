968388551e2e5de9e70fffb6648f4a8fbe9b3d52
Matthew Niemiro <29468575+mattniemiro@users.noreply.github.com>
2025-11-20 18:56:45 -0500

    moved lens again

--- dist/reading3/webgl-morph.js
+++ dist/reading3/webgl-morph.js
@@ -44,7 +44,7 @@ class WebGLMorph {
       lens1Radius: 0.6,
       lens1K1: -1.5,
       lens2X: 0.5,  // Moved 10% left (was 0.6)
-      lens2Y: 1.05,
+      lens2Y: 0.97, // Moved 8% up (was 1.05)
       lens2Radius: 0.35,
       lens2K1: -3.0,
       lens3X: 1.10,
