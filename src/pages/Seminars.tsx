54a42a7932215b8357ec3ff29686859bd601f763
Matthew Niemiro <29468575+mattniemiro@users.noreply.github.com>
2026-02-09 10:16:32 -0500

    Update Seminars.tsx

--- src/pages/Seminars.tsx
+++ src/pages/Seminars.tsx
@@ -5,8 +5,8 @@ export const Seminars = () => {
         Here are some seminars I am organizing or have organized.
       </p>
       <ul className="text-[13px] space-y-2 pl-6">
+        <li>(Spring 2026) <a href="https://freeloop.space/thursday/" className="text-link hover:text-link-hover" target="_blank" rel="noopener noreferrer">Harvard Thursday Seminar</a> --- Semiadditivity learning seminar</li>
         <li>(Spring 2026) <a href="https://freeloop.space/babytop/" className="text-link hover:text-link-hover" target="_blank" rel="noopener noreferrer">Harvard/MIT Babytop</a> --- Trace methods II, joint w/ Kush Singhal</li>
-        <li>(Spring 2026) <a href="https://freeloop.space/thursday/" className="text-link hover:text-link-hover" target="_blank" rel="noopener noreferrer">Harvard Thursday Seminar</a> --- Semiadditivity</li>
         <li>(Fall 2025) <a href="https://freeloop.space/babytop/" className="text-link hover:text-link-hover" target="_blank" rel="noopener noreferrer">Harvard/MIT Babytop</a> --- Trace methods I, joint w/ Logan Hyslop</li>
         <li>(Spring 2025) <a href="https://freeloop.space/babytop/" className="text-link hover:text-link-hover" target="_blank" rel="noopener noreferrer">Harvard/MIT Babytop</a> --- Chromatic homotopy theory</li>
         <li>(Summer 2022) Condensed Seminar @UChicago, joint w/ Michael Barz</li>
